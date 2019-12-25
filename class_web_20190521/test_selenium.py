d46dff813f3f8149f6de233ff033c0356d67eeab
cpmmm <1171954100@qq.com>
2019-12-25 11:16:18 +0800

    update ziyuan

---
+++ class_web_20190521/test_selenium.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+'''
+@time: 2019/12/23 0023 19:13
+@author: chen
+@contact: 1171954100@qq.com
+@file: test_selenium.py
+@desc:
+        ┏┓　　　┏┓+ +
+　　　┏┛┻━━━┛┻┓ + +
+　　　┃　　　　　　　┃ 　
+　　　┃　　　━　　　┃ ++ + + +
+　　 ████━████ ┃+
+　　　┃　　　　　　　┃ +
+　　　┃　　　┻　　　┃
+　　　┃　　　　　　　┃ + +
+　　　┗━┓　　　┏━┛
+　　　　　┃　　　┃　　　　　　　　　　　
+　　　　　┃　　　┃ + + + +
+　　　　　┃　　　┃　　　　Codes are far away from bugs with the animal protecting　　　
+　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug　　
+　　　　　┃　　　┃
+　　　　　┃　　　┃　　+　　　　　　　　　
+　　　　　┃　 　　┗━━━┓ + +
+　　　　　┃ 　　　　　　　┣┓
+　　　　　┃ 　　　　　　　┏┛
+　　　　　┗┓┓┏━┳┓┏┛ + + + +
+　　　　　　┃┫┫　┃┫┫
+　　　　　　┗┻┛　┗┻┛+ + + +
+
+'''
+from selenium import webdriver
+
+#打开谷歌浏览器
+driver = webdriver.Chrome()
+#访问百度首页
+driver.get("http://www.baidu.com")
