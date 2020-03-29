1fbbec3784b6fac7517f5c47d86db659c2ffedff
cpmmm <1171954100@qq.com>
2020-03-29 20:27:06 +0800

    ui first test

---
+++ class_web_20190618/PO_V1/TestDatas/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+'''
+@time: 2020/1/6 0006 16:10
+@author: chen
+@contact: 1171954100@qq.com
+@file: __init__.py.py
+@desc:
+        ┏┓　　　┏┓+ +
+　　　┏┛┻━━━┛┻┓ + +
+　　　┃　　　　　　　┃ 　
+　　　┃　　　━　　　┃ ++ + + +
+　　 ████━████ ┃+
+　　　┃　　　　　　　┃ +
+　　　┃　　　┻　　　┃
+　　　┃　　　　　　　┃ + +
+　　　┗━┓　　　┏━┛
+　　　　　┃　　　┃　　　　　　　　　　　
+　　　　　┃　　　┃ + + + +
+　　　　　┃　　　┃　　　　Codes are far away from bugs with the animal protecting　　　
+　　　　　┃　　　┃ + 　　　　神兽保佑,代码无bug　　
+　　　　　┃　　　┃
+　　　　　┃　　　┃　　+　　　　　　　　　
+　　　　　┃　 　　┗━━━┓ + +
+　　　　　┃ 　　　　　　　┣┓
+　　　　　┃ 　　　　　　　┏┛
+　　　　　┗┓┓┏━┳┓┏┛ + + + +
+　　　　　　┃┫┫　┃┫┫
+　　　　　　┗┻┛　┗┻┛+ + + +
+
+'''
